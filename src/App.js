ff50f1979433ace6be12f125adc72f46bc197d81
boardwallfloor <xanthibumi@gmail.com>
2019-04-25 08:38:16 +0700

    branch stuff

--- src/App.js
+++ src/App.js
@@ -1,44 +1,70 @@
 import React,{Component} from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 //import './App.css';
 import Product from './Product.js';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      products : [
+        {judul:"Charger HaPe",keterangan:"Lorem ipsum sit dolor amet"},
+        {judul:"Charger HaP",keterangan:"Lorem ipsum sit dolor amet"},
+        {judul:"Charger HPe",keterangan:"Lorem ipsum sit dolor amet"}
+    ]
+    }
+  }
+  
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    let name = this.refs.name.value;
+    let desc = this.refs.desc.value;
+    
+
+    console.log(desc ,name);
+  }
+
+  
+  
   render() {
       return (
-    <div class="container">
-        <div class="row">
-          <div class="col s12">
-          <div class="col s12 m4">
-            
-            <div class="card">
-              <div class="card-content">
-                  <form class="container">
+    
+        <div className="row">
+          <div className="col s12">
+          <div className="col s12 m4"> 
+            <div className="card">
+              <div className="card-content">
+                  <form ref="product-form" onSubmit={this.handleSubmit}>
                   <div className="row">
-                    <div class="input-field col s12">
-                      <input placeholder=" Your Name" id="name" type="text" class="validate"></input>
-                      <label for="name">Name</label>
+                    <div className="input-field col s12">
+                      <input placeholder=" Name" ref="name" type="text"/>
+                      <label htmlFor="name">Name</label>
                     </div>
-                    <div class="input-field col s12">
-                      <input placeholder=" Your Name" id="name" type="text" class="validate"></input>
-                      <label for="name">Name</label>
-                    </div>
-                    <div className="center-align">
-                      <a class="waves-effect waves-light blue btn"><i class="material-icons">airplay</i></a>
+                    <div className="input-field col s12">
+                      <input placeholder=" Description" ref="desc" type="text"/>
+                      <label htmlFor="desc">Desc</label>
                     </div>
                     </div>
                   </form>
                 </div> 
+                <div className="card-action">
+                      <button type="submit" name="action" className="waves-effect waves-light blue btn">
+                        <i className="material-icons left">send</i>Submit
+                      </button>
+                </div>
            </div>
            </div>
            <div className="col s12 m8">
-            <Product judul="Charger HP" keterangan="Lorem ipsum sit dolor amet"/>
-            <Product judul="Charger HaP" keterangan="Lorem ipsum sit dolor amet"/>
-            <Product judul="Charger HPe" keterangan="Lorem ipsum sit dolor amet"/>
+            {this.state.products.map((data,key) =>{
+              return (
+                <Product key={key} judul={data.judul} keterangan={data.keterangan}/>
+            )
+            })}
            </div>
         </div>
         </div>
-        </div>
+        
 
       
 );
